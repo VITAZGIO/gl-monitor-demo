1d8006d73ef2827374389dc1f64974b47b585679
VITAZGIO <vitazgio@gmail.com>
2026-04-15 13:33:45 +0300

    Что изменил

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ DEVICES = {
         "connected": True,
         "errors": "Нет",
         "input_pressure": "4.2 бар",
-        "system_pressure": "3.7 бар"
+        "system_pressure": "3.6 бар"
     },
     "GL-002": {
         "connected": False,
